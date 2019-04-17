d0d98421ea9675ee738d22d5870feedaa5b7c7a2
xtoolNH <49430143+xtoolNH@users.noreply.github.com>
2019-04-17 17:23:08 +0530

    Added steup and start files

---
+++ setupFiles/setup.bat
@@ -0,0 +1,12 @@
+ECHO Starting Frontend Installation
+
+ECHO Installing Angular CLI
+call npm install -g @angular/cli
+
+ECHO Installing Angular-Bootstrap
+call npm install @ng-bootstrap/ng-bootstrap
+
+ECHO Installing xToolWeb
+call npm install
+
+ECHO End of Frontend Installation
---
+++ setupFiles/start.bat
@@ -0,0 +1,2 @@
+ECHO Starting Angular Server
+call ng serve -o --port 4300 --host 0.0.0.0
